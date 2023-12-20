e77b46015e81fadb6d87d5ac46aeb3499ed2d09d
MateuszKacperski <kacperskimateusz95@gmail.com>
2023-12-20 14:25:56 +0100

    console print step

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 console.log('Js ok');
+
+
+
+//Preparo il for per stampare i numeri da 1 a 100
+for(let i = 1; i <= 100; i++){
+    let risultato = i;
+    if((i % 3 === 0) && (i % 5 === 0)){
+        risultato = 'FizzBuzz';
+    }else if(i % 3 === 0){
+        risultato = 'Fizz'
+    }else if(i % 5 === 0){
+        risultato = 'Buzz'
+    }
+    console.log(risultato);
+}
