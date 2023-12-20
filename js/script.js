e228211f21d616c357f01decdf18aa83bbd4e923
MateuszKacperski <kacperskimateusz95@gmail.com>
2023-12-20 14:45:02 +0100

    added Bonus 1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 console.log('Js ok');
 
 
+//Prendo il documento sul quale scrivere
+const stampa = document.getElementById('results');
+
+//Creo la lista
+let lista = '<ul>'
 
 //Preparo il for per stampare i numeri da 1 a 100
 for(let i = 1; i <= 100; i++){
@@ -9,8 +14,23 @@ for(let i = 1; i <= 100; i++){
         risultato = 'FizzBuzz';
     }else if(i % 3 === 0){
         risultato = 'Fizz'
+    
     }else if(i % 5 === 0){
         risultato = 'Buzz'
+        
     }
     console.log(risultato);
+
+
+    // aggiungo gli li alla lista
+    lista += `<li>${risultato}</li>`
+
+
+
 }
+
+    //chiudo la lista
+    lista += '</ul>'
+
+    stampa.innerHTML = lista;
+
