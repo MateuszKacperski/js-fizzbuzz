e018cf363503d84373b90e1860d33bbcf4493770
MateuszKacperski <kacperskimateusz95@gmail.com>
2023-12-21 10:25:41 +0100

    changing class name

--- js/script.js
+++ js/script.js
@@ -11,12 +11,12 @@ let lista = '<ul>'
 for(let i = 1; i <= 100; i++){
     let risultato = i;
     if((i % 3 === 0) && (i % 5 === 0)){
-        risultato = 'FizzBuzz';
+        risultato = 'fizzbuzz';
     }else if(i % 3 === 0){
-        risultato = 'Fizz';
+        risultato = 'fizz';
     
     }else if(i % 5 === 0){
-        risultato = 'Buzz';
+        risultato = 'buzz';
         
     }
     console.log(risultato);
